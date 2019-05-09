9c2094027be5fc459f1621b3f7bcd025cc4b17ce
Toni Stapleton <tonistpltn25@gmail.com>
2019-05-09 15:58:04 -0500

    edit slides

--- js/plot.js
+++ js/plot.js
@@ -1,4 +1,4 @@
-Plotly.d3.csv('Under_5_Mortality_2017.csv', function(err, rows){
+Plotly.d3.csv('Under_5_Mortality_2017_withcountries.csv', function(err, rows){
       function unpack(rows, key) {
           return rows.map(function(row) { return row[key]; });
       }
@@ -22,19 +22,30 @@ Plotly.d3.csv('Under_5_Mortality_2017.csv', function(err, rows){
             dtick: 1000,
             colorbar: {
                 autotic: false,
-                title: 'Child Mortality Rate: Deaths per 1000'
+                title: 'Child Mortality Rate:<br>Deaths per 1000 Live Births',
+                titlefont: {
+                    family: 'Karla-Regular',
+                    size: 20,
+                    // color: '#547980'
+                  },
             }
       }];
 
       var layout = {
           title: '<b>2017 Under 5 Child Mortality Rate</b><br>Source: <a href="https://data.unicef.org/topic/child-survival/under-five-mortality/"> UNICEF</a>',
+          titlefont: {
+            family: 'Karla-Regular',
+            size: 30,
+            color: '#547980'
+          },
           geo:{
               showframe: false,
               showcoastlines: false,
               projection:{
                   type: 'mercator'
               }
-          }
+          },
+    
       };
       Plotly.plot('map', data, layout);
 });
